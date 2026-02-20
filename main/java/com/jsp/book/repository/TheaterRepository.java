2aaf6ce83daf697ad01a605549826b984b61de6d
Bhoomi593 <bhoomijeev@gmail.com>
2026-02-20 20:16:58 +0530

    7th commit

---
+++ main/java/com/jsp/book/repository/TheaterRepository.java
@@ -0,0 +1,10 @@
+package com.jsp.book.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jsp.book.entity.Theater;
+
+public interface TheaterRepository extends JpaRepository<Theater, Long> {
+
+	boolean existByNameAndAddress(String name, String address);
+}
